97de0f247706700c6a2fb826bab398a408169ea4
g-veronika <g.veronika2021@gmail.com>
2023-01-07 18:17:42 +0100

    update layout

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,19 +1,46 @@
 <template>
   <main>
     <h1>Roman numerals</h1>
-
+    <p>Enter an integer number from 0 to 100</p>
+    <form>
+      <input type="number" name="number-form" required min="0" max="100" />
+      <button type="submit">Convert</button>
+      <div class="container-result">
+        <div>
+        </div>
+      </div>
+    </form>
   </main>
-
-  
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
+<style lang="scss">
 
-</script>
+html {
+  font-family: effra;
+  background-color: #f8f5f1;
+}
 
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-<style scoped>
+  h1 {
+    margin-top: 3rem;
+  }
+  
+  p {
+    font-size: 20px;
+  }
 
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+  
+}
 
 </style>
